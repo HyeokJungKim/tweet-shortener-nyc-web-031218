c0c5d60204ff2190d00a0ae0302f50cd8b9be718
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-01 07:03:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,3 +42,11 @@ def selective_tweet_shortener(tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    tweet[0..139]
+  else
+    word_substituter(tweet)
+  end
+end
